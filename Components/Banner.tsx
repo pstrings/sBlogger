7159a597e8f6eed62164458e1ccdc95daed4f58d
Suyash <paradisestrings99@gmail.com>
2023-02-27 18:14:33 +0530

    cleaning up header

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -3,7 +3,7 @@ import sLogo from "../public/sLogo.png";
 
 const Banner = () => {
   return (
-    <div className="flex">
+    <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
       <div className="px-10 space-y-5">
         <h1 className="text-6xl max-w-xl font-serif">
           <span className="underline decoration-4">sBlogger</span> is a place to
--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,34 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-// import logo from "../public/logo.png";
 import logo from "../public/sBloggerLogo.png";
 
 function Header() {
   return (
-    // background color to match logo: #2c3032
-    <header className="flex justify-between px-5 max-w-7xl mx-auto bg-gray-500">
-      <div className="flex items-center space-x-5">
-        <Link href="/">
-          <Image
-            className="w-40 object-contain cursor-pointer"
-            src={logo}
-            alt="Logo"
-          />
-        </Link>
-      </div>
-      <div className="flex items-center space-x-5">
-        <nav className="hidden md:inline-flex items-center space-x-5">
-          <Link href="#">About</Link>
-          <Link href="#">Contact</Link>
-          <Link
-            href="#"
-            className="text-white bg-green-600 px-4 py-1 rounded-full"
-          >
-            Subscribe
+    <div>
+      <header className="flex justify-between px-5 max-w-7xl mx-auto">
+        <div className="flex items-center space-x-5">
+          <Link href="/">
+            <Image
+              className="w-40 object-contain cursor-pointer"
+              src={logo}
+              alt="Logo"
+            />
           </Link>
-        </nav>
-      </div>
-    </header>
+        </div>
+        <div className="flex items-center space-x-5">
+          <nav className="hidden md:inline-flex items-center space-x-5">
+            <Link href="#">About</Link>
+            <Link href="#">Contact</Link>
+            <Link
+              href="#"
+              className="text-white bg-green-600 px-4 py-1 rounded-full"
+            >
+              Subscribe
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 }
 
