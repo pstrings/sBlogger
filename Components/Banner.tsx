0e5806b54814df130d681f94f66d5f8e433c3658
Suyash <paradisestrings99@gmail.com>
2023-02-27 18:01:42 +0530

    made changes to header

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import sLogo from "../public/sLogo.png";
+
 const Banner = () => {
   return (
-    <>
+    <div className="flex">
       <div className="px-10 space-y-5">
         <h1 className="text-6xl max-w-xl font-serif">
           <span className="underline decoration-4">sBlogger</span> is a place to
@@ -8,8 +11,12 @@ const Banner = () => {
         </h1>
         <h3>It is easy and free.</h3>
       </div>
-      <div></div>
-    </>
+      <Image
+        className="hidden md:inline-flex w-20 h-32 lg:h-48 lg:w-44"
+        src={sLogo}
+        alt="Image of letter S"
+      />
+    </div>
   );
 };
 
--- Components/Header.tsx
+++ Components/Header.tsx
@@ -15,19 +15,18 @@ function Header() {
             alt="Logo"
           />
         </Link>
-        <nav className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
-            Follow
-          </h3>
-        </nav>
       </div>
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3 className="hidden md:inline-flex">Sign In</h3>
-        <h3 className="border border-green-600 px-4 py-1 rounded-full">
-          Get Started
-        </h3>
+      <div className="flex items-center space-x-5">
+        <nav className="hidden md:inline-flex items-center space-x-5">
+          <Link href="#">About</Link>
+          <Link href="#">Contact</Link>
+          <Link
+            href="#"
+            className="text-white bg-green-600 px-4 py-1 rounded-full"
+          >
+            Subscribe
+          </Link>
+        </nav>
       </div>
     </header>
   );
