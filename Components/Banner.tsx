84a29c9c58dabd6288bbc1b672424530187032e7
Suyash <paradisestrings99@gmail.com>
2023-02-27 23:35:30 +0530

    banner content change

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -6,10 +6,12 @@ const Banner = () => {
     <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
       <div className="px-10 space-y-5">
         <h1 className="text-6xl max-w-xl font-serif">
-          <span className="underline decoration-4">sBlogger</span> is a place to
-          write, read, and connect
+          <span className="underline decoration-4">sBlogger</span> is a place
+          where I write everything and anything.
         </h1>
-        <h3>It is easy and free.</h3>
+        <h3>
+          What is the theme of my blog? Nothing! Themes kills creativity!!
+        </h3>
       </div>
       <Image
         className="hidden md:inline-flex w-20 h-32 lg:h-48 lg:w-44"
