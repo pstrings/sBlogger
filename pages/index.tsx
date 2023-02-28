233857e10276e287ed2e5bf2da18c019069e818f
Suyash <paradisestrings99@gmail.com>
2023-03-01 01:19:14 +0530

    final adjustment to posts on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,19 +42,36 @@ export default function Home({ posts }: { posts: Post }) {
       <Banner />
       <main>{/* Posts */}</main>
       {/* have to wrap the posts in fragment otherwise we'll get type error */}
-      <div>
-        <>
-          {posts.map((post) => (
-            <Link key={post._id} href={`/post/${post.slug.current}`}>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
               <Image
-                height={300}
-                width={300}
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                height={1000}
+                width={1000}
                 src={urlFor(post.mainImage).url()!}
                 alt={`${post.title} image`}
               />
-            </Link>
-          ))}
-        </>
+              <div className="flex justify-between p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <Image
+                  className="rounded-full h-12 w-12"
+                  src={urlFor(post.author.image).url()!}
+                  alt={`${post.author.name}'s image`}
+                  height={50}
+                  width={50}
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
