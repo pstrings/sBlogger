91773bd1149f8eb8a2bf0fd41a1113daada84056
Suyash <paradisestrings99@gmail.com>
2023-03-02 19:49:17 +0530

    reverted import syntax

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "../components/header";
+import Header from "@/components/header";
 import Banner from "@/components/banner";
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
